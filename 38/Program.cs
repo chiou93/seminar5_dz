c914b99a1bcd2cc4adcfcccddd0e51fb58681287
Leisan <dororoqw@gmail.com>
2022-09-04 05:10:32 +0300

    +

--- 38/Program.cs
+++ 38/Program.cs
@@ -1,44 +1,82 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-double[] numbers = new double[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-double min = Int32.MaxValue;
-double max = Int32.MinValue;
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// double[] numbers = new double[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Массив: ");
+// PrintArray(numbers);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
 
-for (int z = 0; z < numbers.Length; z++)
-{
-    if (numbers[z] > max)
-        {
-            max = numbers[z];
-        }
-    if (numbers[z] < min)
-        {
-            min = numbers[z];
-        }
-}
+// for (int z = 0; z < numbers.Length; z++)
+// {
+//     if (numbers[z] > max)
+//         {
+//             max = numbers[z];
+//         }
+//     if (numbers[z] < min)
+//         {
+//             min = numbers[z];
+//         }
+// }
 
-Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
-Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
+// Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max}, минимальное значение = {min}");
+// Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
 
-void FillArrayRandomNumbers(double[] numbers)
+// void FillArrayRandomNumbers(double[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(double[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < numbers.Length; i++)
+//         {
+//             Console.Write(numbers[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+
+class Program
 {
-    for(int i = 0; i < numbers.Length; i++)
+    static int MaxNumberArray(int[] array)
+    {
+        int max = 0;
+        for (int i = 0; i < array.Length; i++)
         {
-            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
         }
-}
-void PrintArray(double[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+        return max;
+    }
+    static int MinNumberArray(int[] array)
+    {
+        int min = array[0];
+        for (int i = 0; i < array.Length; i++)
         {
-            Console.Write(numbers[i] + " ");
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
         }
-    Console.Write("]");
-    Console.WriteLine();
+        return min;
+    }
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 56, 678, 124, 3 };
+        int max = MaxNumberArray(array);
+        int min = MinNumberArray(array);
+        Console.WriteLine(max);
+        Console.WriteLine(min);
+        Console.WriteLine(max - min);
+    }
 }
